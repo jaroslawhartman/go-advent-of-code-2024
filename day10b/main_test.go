0c6b8dbce30f7a47536c53b585e276ecf9751a4d
Jaroslaw Hartman <jaroslaw.hartman@oracle.com>
2024-12-20 21:53:48 +0100

    Day 10b

---
+++ day10b/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	got := run("input_test.txt")
+	expected := 81
+	if got != expected {
+		t.Errorf("Run = %d; want %d", got, expected)
+	}
+}
